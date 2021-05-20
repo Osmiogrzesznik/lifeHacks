abd12276a7c4bcd9e6a1efdd3154290605c24515
SourceCowD <boleskok@gmail.com>
2021-05-20 18:00:16 +0100

    Add files via upload

--- script for Youtube Referencing.js
+++ script for Youtube Referencing.js
@@ -37,11 +37,11 @@ function fullMonth(p_dateStr) {
     return day + " " + month;
 }
 
-const italics = x => "<i>" + x + "</i>";
-const brackets = x => `(${x})`;
-const capitalize = x => x.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-const getYTcode = x => x.split(['v='])[1].split("&")[0]
-const ytbe = x => "https://youtu.be/" + getYTcode(x);
+let italics = x => "<i>" + x + "</i>";
+let brackets = x => `(${x})`;
+let capitalize = x => x.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+let getYTcode = x => x.split(['v='])[1].split("&")[0]
+let ytbe = x => "https://youtu.be/" + getYTcode(x);
 
 function getCurRndDate(lastMonths) {
     let monthNames = ["January", "February", "March", "April", "May", "June",
@@ -64,7 +64,12 @@ nameOfPersonPostingVideo = x => x;
 yearVideoPosted = x => brackets(x.substr(-4));
 titleOfVideo = x => capitalize(x) + ".";
 dateUploaded = x => fullMonth(x) + ".";
-availableAt = x => "Available at: " + ytbe(document.location.href);
+availableAt = x => {
+    ytc = ytbe(document.location.href)
+    sa = "Available at: " + ytbe(document.location.href);
+    htmla = `<a href='${ytc}'>${ytc}</a>`
+    return htmla;
+}
 accessed = x => brackets("Accessed: " + getCurRndDate(3)) + ".";
 
 outArr = [
@@ -77,6 +82,32 @@ outArr = [
 ]
 
 out = outArr.join(" ")
+
+d = document.createElement("div")
+d.style.zIndex = "9999999"
+d.style.position = "fixed"
+d.style.left = "0px"
+d.style.top = "0px"
+d.style.background = "white"
+d.style.height = "100px"
+d.style.padding = "20px"
+d.innerHTML = out;
+document.body.append(d)
+
+d.addEventListener('click', function () {
+    if (document.selection) { // IE
+        var range = document.body.createTextRange();
+        range.moveToElementText(d);
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(d);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+    }
+})
+
+
 /*
 Name of person posting video 
 (Year video posted) 
